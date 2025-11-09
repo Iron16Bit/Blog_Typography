3aa0e867cdd430902ec5cb76f2a408acf316a8ff
Salvatore Gilles Cassarà <gilles2004.sc@gmail.com>
2025-11-09 12:44:36 +0100

    Init

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,7 +1,57 @@
-import type { UserConfig } from '~/types'
+import type { UserConfig } from "~/types";
 
 export const userConfig: Partial<UserConfig> = {
-  // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
-}
+  site: {
+    title: "Aeroplanini Di Carta",
+    subtitle: "",
+    author: "Iron16Bit",
+    description: "Parole espresse al vento, pensieri liberi di fluire",
+    pageSize: 5,
+    socialLinks: [
+      {
+        name: "github",
+        href: "https://github.com/Iron16Bit",
+      },
+    ],
+    navLinks: [
+      {
+        name: "Posts",
+        href: "/",
+      },
+      {
+        name: "Archive",
+        href: "/archive",
+      },
+      {
+        name: "Categories",
+        href: "/categories",
+      },
+      {
+        name: "About",
+        href: "/about",
+      },
+    ],
+    categoryMap: [{ name: "胡适", path: "hu-shi" }],
+    footer: [
+      '© %year <a target="_blank" href="%website">%author</a>',
+      'Original Theme <a target="_blank" href="https://github.com/Moeyua/astro-theme-typography">Typography</a> by <a target="_blank" href="https://moeyua.com">Moeyua</a>',
+    ],
+  },
+  appearance: {
+    theme: "system",
+    locale: "it-it",
+    colorsLight: {
+      primary: "#2e405b",
+      background: "#ffffff",
+    },
+    colorsDark: {
+      primary: "#FFFFFF",
+      background: "#232222",
+    },
+    fonts: {
+      header:
+        '"HiraMinProN-W6","Source Han Serif CN","Source Han Serif SC","Source Han Serif TC",serif',
+      ui: '"Source Sans Pro","Roboto","Helvetica","Helvetica Neue","Source Han Sans SC","Source Han Sans TC","PingFang SC","PingFang HK","PingFang TC",sans-serif',
+    },
+  },
+};
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,6 +1,6 @@
 ---
-title: 关于
-pubDate: 2024-12-06
+title: Chi sono?
+pubDate: 09-11-2025
 ---
 
-此主题系 Hexo 主题 <a href="https://github.com/sumimakito/hexo-theme-typography">活版印字</a> 在 Astro 平台上的移植版本
+Sono uno studente magistrale di informatica. Nel mio tempo libero adoro leggere, guardare film e giocare ai videogiochi. Tra lo studio e le mie varie passione sicuramente non è difficile trovare il modo di occupare il mio tempo, ma tramite un'amica ho recentemente notato una cosa: indipendentemente dalla loro qualità, tutto ciò che faccio è subire più o meno passivamente. <ins>Adoro</ins> ciò che è stato creato da altri, ma non dono nulla a mia volta. Questo blog nasce proprio da questo pensiero. L'importante non è che queste parole arrivino a qualcuno, ma che vengano espresse. Un giorno magari diventeranno qualcosa di più. Magari no. Intanto prendono forma, diventano lettere su carta che diventerà a sua volta un aeroplanino affidato al vento.
